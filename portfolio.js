028a2b5fd26491a6b9f2a2357678041a014d35be
Aniket Virendrapratap Singh <158837312+Singhaniket2110@users.noreply.github.com>
2024-02-22 18:19:24 +0530

    JAVASCRIPT CODE

---
+++ portfolio.js
@@ -0,0 +1,7 @@
+var typed = new Typed (".text1", {
+    Strings : [" I'M student " , " FY BSC IT " , " 2005 " ],
+    typeSpeed : 100,
+    backSpeed : 100,
+    backDelay : 1000,
+    loop : true
+});
